4d0661b557990f3d953288877577cf022b62c2f4
Anthony Dominguez <Anthonydv7@outlook.com>
2019-08-13 23:20:04 -0500

    have to figure out how to get the stopped the version of the URL.

--- ApiHomework/api.js
+++ ApiHomework/api.js
@@ -1,25 +1,68 @@
 const buttonsArray = ["lazers", "lights", "bass", "skateboard"];
 
-const submitButton = document.getElementById("submit-button"); 
+const submitButton = document.getElementById("submit-button");
 
 
-function showGifs(){
-        
+function showGifs() {
+
     const userInput = this.getAttribute("data-name");
 
     const queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-      userInput + "&api_key=G4QzXGn24vfsHW4XQfzt2aNZdHBRRhRK&limit=10";
+        userInput + "&api_key=G4QzXGn24vfsHW4XQfzt2aNZdHBRRhRK&limit=10";
 
 
-    fetch(queryURL).then(function(response) {
-      return response.json();
-    }).then(function(responseJson) {
+    fetch(queryURL).then(function (response) {
+        return response.json();
+    }).then(function (responseJson) {
 
-      console.log(responseJson.data[0])
+        const results = responseJson.data;
 
+        for (let i = 0; i < results.length; i++) {
+
+            if (results[i].rating === "r" || results[i].rating === "pg") {
+                console.log(responseJson)
+
+                const gifDiv = document.createElement("div");
+
+                const rating = results[i].rating;
+
+                const p = document.createElement("p");
+                p.innerHTML = "Rating: " + rating;
+
+                const userImage = document.createElement("img");
+               const still = "still"
+                userImage.setAttribute("src", results[i].images.fixed_height.url);
+                userImage.setAttribute("data-still", results[i].images.fixed_height.url)
+                userImage.setAttribute("data-animate", results[i].images.fixed_height.url)
+                userImage.setAttribute("data-state", still);
+               
+                userImage.classList.add("gif");
+
+                gifDiv.append(p);
+                gifDiv.append(userImage)
+
+                document.getElementById("gif-spot").prepend(gifDiv);
+                document.querySelectorAll(".gif").forEach(function (img) {
+                    img.addEventListener("click", function (event) {
+              
+                    
+                      var state = event.target.getAttribute("data-state");
+                   
+                      if (state === "still") {
+                        event.target.setAttribute("src", event.target.getAttribute("data-animate"));
+                        event.target.setAttribute("data-state", "animate");
+                      } else {
+                        event.target.setAttribute("src", event.target.getAttribute("data-still"));
+                        event.target.setAttribute("data-state", "still");
+                      }
+                    });
+                  });
+            }
+        }
     });
-  }
-  
+}
+
+
 
 function renderButtons() {
 
@@ -38,13 +81,13 @@ function renderButtons() {
         document.getElementById("buttons-spot").append(newButton);
 
         newButton.addEventListener("click", showGifs)
-        
-        };
 
-    
-    }
+    };
 
-document.getElementById("submit-button").addEventListener("click", function(event){
+
+}
+
+document.getElementById("submit-button").addEventListener("click", function (event) {
     event.preventDefault();
 
     const gifRequest = document.getElementById("user-input").value.trim();
@@ -57,24 +100,6 @@ document.getElementById("submit-button").addEventListener("click", function(even
 
 renderButtons()
 
-// submitButton.addEventListener("click", function () {
-
-//     const apiKey = 'G4QzXGn24vfsHW4XQfzt2aNZdHBRRhRK';
-
-//     const userInput = document.getElementById("user-input").value.trim();
-
-//     const queryURL = "https://api.giphy.com/v1/gifs/search?q=" + userInput + "&api_key=" + apiKey;
-
-
-
-
-//     event.preventDefault();
-
-//     fetch(queryURL)
-//         .then(function (response) {
-//             return response.json();
-//         })
-//         .then(function (responseJson) {
 
 
 //             const results = responseJson.data;
